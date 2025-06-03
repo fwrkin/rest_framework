d9249ce6190b16277d71cac4582a964b00c9a554
fwrkin <fedotovvva23@mail.ru>
2025-06-03 14:15:17 +0300

    35.2 rework

--- config/settings.py
+++ config/settings.py
@@ -60,16 +60,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": os.getenv("HOST"),
-        "PORT": os.getenv("PORT"),
+
+if os.getenv("TEST_ENV"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "test_db.sqlite3"),
+        }
     }
-}
+else:
+    DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.postgresql_psycopg2",
+                "NAME": os.getenv("NAME"),
+                "USER": os.getenv("USER"),
+                "PASSWORD": os.getenv("PASSWORD"),
+                "HOST": os.getenv("HOST"),
+                "PORT": os.getenv("PORT"),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
